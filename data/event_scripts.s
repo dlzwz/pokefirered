ce35a69878ddaab780d21c52381f2cb3064bc2aa
dlzwz <da.lobato95@gmail.com>
2026-01-03 17:28:44 -0500

    Overworld 1HP Poison Survival

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -882,7 +882,8 @@ Text_GiveNicknameToReceivedMon::
 	.string "{STR_VAR_2} you received?$"
 
 gText_PkmnFainted3::
-	.string "{STR_VAR_1} fainted…\p"
+	.string "{STR_VAR_1} survived the poisoning.\n"
+	.string "The poison faded away!\p"
 	.string "$"
 
 Text_WelcomeWantToHealPkmn::
